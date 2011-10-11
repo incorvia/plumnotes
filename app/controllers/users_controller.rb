b8b452a5b2e2fa799e19a50fd6ef65f15aa276ac
incorvia <incorvia@gmail.com>
2011-10-10 20:33:17 -0400

    fixed formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,9 +40,7 @@ private
 
   def welcome
     @note = current_user.notecards.create(:content => 
-      "Welcome to your new note-taking application! \n \n A few helpful points:  When creating a note, 
-      type \"Shift + Enter\" to enter a line break.  To save your note just hit \"enter\".  Tags can be
-      entered by preceding a word with a \"@\" symbol like: @welcome.  
+      "Welcome to your new note-taking application! \n \n A few helpful points:  When creating a note, type \"Shift + Enter\" to enter a line break.  To save your note just hit \"enter\".  Tags can be entered by preceding a word with a \"@\" symbol like: @welcome.  
       \n \n
       Enjoy your new found productivity!" )
     @note.tags.create(:tag_name => "@welcome")
